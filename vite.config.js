ed72e620c9c72aca2d6874006c4f9c9823792f31
promisecaring <poghene@byupathway.edu>
2026-09-24 19:38:27 +0100

    Add checkout success page to Vite build

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
+         checkout: resolve(__dirname, "src/checkout/success.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
         productListing: resolve(__dirname, "src/product_listing/index.html"),
       },
